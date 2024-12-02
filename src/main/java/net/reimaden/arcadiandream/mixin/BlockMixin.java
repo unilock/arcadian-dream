250d7f65c189dc1cc1096ac2054c4c70db6eef4a
unilock <unilock@fennet.rentals>
2024-12-02 01:35:00 -0500

    do not unconditionally cancel BlockMixin#arcadiandream$getDroppedStacks
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/net/reimaden/arcadiandream/mixin/BlockMixin.java
+++ src/main/java/net/reimaden/arcadiandream/mixin/BlockMixin.java
@@ -22,7 +22,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -54,34 +53,25 @@ import java.util.Optional;
 @Mixin(Block.class)
 public class BlockMixin {
 
-    @Inject(method = "getDroppedStacks(Lnet/minecraft/block/BlockState;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/entity/Entity;Lnet/minecraft/item/ItemStack;)Ljava/util/List;", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "getDroppedStacks(Lnet/minecraft/block/BlockState;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/entity/Entity;Lnet/minecraft/item/ItemStack;)Ljava/util/List;", at = @At("RETURN"))
     private static void arcadiandream$getDroppedStacks(BlockState state, ServerWorld world, BlockPos pos, BlockEntity blockEntity, Entity entity, ItemStack stack, CallbackInfoReturnable<List<ItemStack>> cir) {
-        List<ItemStack> items = new ArrayList<>();
-        List<ItemStack> returnValue = cir.getReturnValue();
-
         if (!(stack.getItem() instanceof HihiirokaneToolItem)) {
-            cir.setReturnValue(returnValue);
             return;
         }
 
+        List<ItemStack> drops = cir.getReturnValue();
         NbtCompound nbt = stack.getOrCreateNbt();
         if (!nbt.contains("autosmelt") || nbt.getBoolean("autosmelt")) {
-            for (ItemStack itemStack : returnValue) {
+            for (int i = 0; i < drops.size(); i++) {
+                ItemStack itemStack = drops.get(i);
                 Optional<SmeltingRecipe> recipe = world.getRecipeManager().getFirstMatch(RecipeType.SMELTING, new SimpleInventory(itemStack), world);
 
                 if (recipe.isPresent()) {
                     ItemStack smelted = recipe.get().getOutput(world.getRegistryManager()).copy();
                     smelted.setCount(itemStack.getCount());
-                    items.add(smelted);
-                } else {
-                    items.add(itemStack);
+                    drops.set(i, smelted);
                 }
             }
-
-            cir.setReturnValue(items);
-            return;
         }
-
-        cir.setReturnValue(returnValue);
     }
 }
